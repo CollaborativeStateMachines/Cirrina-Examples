02485720f38c88269de210d726a8fdeb512f73cd
AlexZangerle <alex.z12angerle1@gmail.com>
2026-02-24 10:23:10 +0100

    refactor: cleaning up chameneos

--- chameneos/run/run.py
+++ chameneos/run/run.py
@@ -1,36 +1,35 @@
 import zenoh
 import time
-import asyncio
 import Event_pb2
 import uuid
 
-async def main():
-
+def main():
     with zenoh.open(zenoh.Config()) as session:
-            evt = Event_pb2.Event(
-                    topic="start",
-                    channel=Event_pb2.Event.PERIPHERAL,
-                    data=[],
-                    target="",
-                    source="",
-                    id=str(uuid.uuid4()),
-                    createdTime=int(time.time() * 1000),
-                )
-            start = time.time()
-            session.put(
-                "events/peripheral/start",
-                evt.SerializeToString(),
-                encoding=zenoh.Encoding.APPLICATION_PROTOBUF,
-            )
-            print(f"Published: {evt}")
+        evt = Event_pb2.Event(
+            topic="start",
+            channel=Event_pb2.Event.PERIPHERAL,
+            data=[],
+            target="",
+            source="",
+            id=str(uuid.uuid4()),
+            createdTime=int(time.time() * 1000),
+        )
 
-            def listener(sample: zenoh.Sample):
-                end = time.time()
-                print(f"Done. Actual Duration: {end - start:.4f} seconds")
+        start = time.time()
+        session.put(
+            "events/peripheral/start",
+            evt.SerializeToString(),
+            encoding=zenoh.Encoding.APPLICATION_PROTOBUF,
+        )
+        print(f"Published: {evt}")
 
-            session.declare_subscriber("events/mall/done", listener)
-            while True:
-                time.sleep(0.01)
+        def listener(sample: zenoh.Sample):
+            end = time.time()
+            print(f"Done. Actual Duration: {end - start:.4f} seconds")
+
+        session.declare_subscriber("events/mall/done", listener)
+
+        input("Press Enter to quit\n")
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
